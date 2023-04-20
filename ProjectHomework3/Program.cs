f0e98fd9d0bb56762d1493659a0d497e4738a3e0
Daniela <97022507+rdanielaanamaria@users.noreply.github.com>
2023-04-20 13:43:54 +0300

    Initial commit

---
+++ ProjectHomework3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHomework3
+{
+    public class Program3
+    {
+        public static void Main(string[] args)
+        {
+            int OldIndex, NewIndex;
+            Console.WriteLine("Enter the old index: ");
+            OldIndex = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the new index: ");
+            NewIndex = int.Parse(Console.ReadLine());
+
+            Utilities Utilities1 = new Utilities();
+            Console.WriteLine("The energy in kWh is: " + Utilities1.EnergyBill(OldIndex, NewIndex));
+            Thread.Sleep(10000);
+
+        }
+    }
+}
---
+++ ProjectHomework3/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHomework3
+{
+    public class Utilities
+    {
+        
+        public int EnergyBill(int OldIndex, int NewIndex)
+        {
+            if (NewIndex - OldIndex > 0)
+                return NewIndex - OldIndex;
+            else
+                return OldIndex-NewIndex;
+            
+        }
+    }
+}
